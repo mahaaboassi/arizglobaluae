87bedc80dff5423a29549a392ddd5c036cb089e6
Maha Abo Assi <maha@onekw.com>
2025-05-04 02:59:32 +0400

    add helmet for 404 page

--- src/pages/404page.jsx
+++ src/pages/404page.jsx
@@ -1,5 +1,13 @@
+import { Helmet } from "react-helmet-async";
+
 const Page_404 = ()=>{
     return <div className="page-404">
+            <Helmet>
+                <title>404 - Page Not Found | Ariz Global</title>
+                <meta name="robots" content="noindex, nofollow" />
+                <meta name="description" content="Sorry, the page you’re looking for doesn’t exist. Please check the URL or return to the homepage." />
+                {/* <link rel="canonical" href="https://arizglobal.com/404" /> */}
+            </Helmet>
             <div className="lines">
                 {Array.from({ length: 1000 }).map((_, idx) => {
                         const height = Math.floor(Math.random() * 400) + 100; // 100px to 500px
